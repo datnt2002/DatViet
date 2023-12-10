34000889b2fcbd24d24613f315babbbff95b7800
ducntapp <ducntapp@gmail.com>
2023-12-10 20:01:57 +0700

    add img

---
+++ fe/src/pages/puzzle/constants.js
@@ -0,0 +1,42 @@
+export const imgList = [
+  {
+    id: 1,
+    name: "Cột cờ lũng cú",
+    src: "../../assets/imgs/game/cot_co_lung_cu.png",
+  },
+  {
+    id: 2,
+    name: "Khuê văn các",
+    src: "../../assets/imgs/game/khue_van_cac.png.png",
+  },
+  {
+    id: 3,
+    name: "Lăng Bác",
+    src: "../../assets/imgs/game/lang_bac.png",
+  },
+  {
+    id: 4,
+    name: "Nhà Rồng",
+    src: "../../assets/imgs/game/nha_rong.png",
+  },
+  {
+    id: 5,
+    name: "Thánh Địa Mỹ Sơn",
+    src: "../../assets/imgs/game/thanh_dia_my_son.png",
+  },
+  {
+    id: 6,
+    name: "Tp Hồ Chí Minh",
+    src: "../../assets/imgs/game/tp_hcm.png",
+  },
+  {
+    id: 7,
+    name: "Tượng Lý Thái Tổ",
+    src: "../../assets/imgs/game/tuong_ly_thai_to.png",
+  },
+  {
+    id: 8,
+    name: "Vịnh Hạ Long",
+    src: "../../assets/imgs/game/vinh_ha_long.png",
+  },
+];
