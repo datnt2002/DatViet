f643c42b94ed2e4cbb99e38b3c48aaf16e77fdbf
Nguyễn Trọng Đạt <103309042+datnt2002@users.noreply.github.com>
2023-12-15 03:06:45 +0700

    vuot chuong ngia vat

--- fe/src/pages/obstacle/index.jsx
+++ fe/src/pages/obstacle/index.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./gamestyle.css";
 import { Badge, Typography, List, Flex, Button } from "antd";
 import { crosswordData } from "./data/dummy_cross";
 import { BulbOutlined } from "@ant-design/icons";
+import { useNavigate } from "react-router-dom";
 
 const { Text } = Typography;
 
@@ -18,14 +19,17 @@ const calSpace = (typedAnswer) => {
 };
 
 const Obstacle = () => {
+  const [completedRows, setCompletedRows] = useState([]);
+  const [isCompleted, setIsCompleted] = useState(false);
+  const navigate = useNavigate();
   useEffect(() => {
-
     return () => {};
   }, []);
 
   const renderRowItem = (item, index) => {
     let crossInputs = [];
     let spaceLeft = calSpace(item.typedAnswer);
+
     for (let index = 1; index <= item?.answer?.length; index++) {
       crossInputs.push(index);
     }
@@ -42,13 +46,30 @@ const Obstacle = () => {
           }
         });
       }
-      if (currentAnswer === item.answer) {
-        document.getElementById(rowId).className = "";
-        document.getElementById(rowId).classList.add("cross-correct");
+      if (currentAnswer === crosswordData[index].answer) {
+        document
+          .getElementById(`cross-row${index}`)
+          .classList.add("cross-correct");
+        const newCompletedRows = [...completedRows, index];
+        setCompletedRows(newCompletedRows);
+        if (newCompletedRows.length === crosswordData.length) {
+          // All rows are completed, show the button
+          // You can add your logic here
+          console.log("All rows completed!");
+          setIsCompleted(true);
+        }
       } else {
-        document.getElementById(rowId).className = "";
-        document.getElementById(rowId).classList.add("cross-wrong");
+        document
+          .getElementById(`cross-row${index}`)
+          .classList.add("cross-wrong");
       }
+      // if (currentAnswer === item.answer) {
+      //   document.getElementById(rowId).className = "";
+      //   document.getElementById(rowId).classList.add("cross-correct");
+      // } else {
+      //   document.getElementById(rowId).className = "";
+      //   document.getElementById(rowId).classList.add("cross-wrong");
+      // }
     };
     const handleOnKeyUp = (event) => {
       if (
@@ -112,7 +133,7 @@ const Obstacle = () => {
 
   return (
     <div className="flex flex-col items-center">
-      <h4 className="font-dancing text-3xl mb-2">Vượt chướng ngại vật</h4>
+      <h4 className="font-dancing text-4xl mb-8 mt-6">Vượt chướng ngại vật</h4>
       <div className="crossword-container">
         <List
           itemLayout="horizontal"
@@ -121,19 +142,46 @@ const Obstacle = () => {
         />
       </div>
       <div>
-        <h5 className="text-center font-dancing text-2xl">Bộ Câu Hỏi</h5>
-        <List
-          itemLayout="horizontal"
-          dataSource={crosswordData}
-          renderItem={(row, index) => (
-            <li key={row.typedAnswer}>
-              <div className="flex flex-row">
-                <Badge count={index + 1} color="#faad14" />
-                <Text className="ml-2">{row.question}</Text>
-              </div>
-            </li>
+        <h5 className="text-center font-dancing text-3xl my-6">Bộ Câu Hỏi</h5>
+        <div className="mb-10">
+          <List
+            itemLayout="horizontal"
+            dataSource={crosswordData}
+            renderItem={(row, index) => (
+              <li key={row.typedAnswer} className="my-2">
+                <div className="flex flex-row">
+                  <Badge count={index + 1} color="#faad14" />
+                  <Text className="ml-2 text-2xl font-dancing">
+                    {row.question}
+                  </Text>
+                </div>
+              </li>
+            )}
+          />
+          {isCompleted && (
+            <div className="mx-auto mt-4 w-fit">
+              <button
+                className="p-4 bg-green-300 mx-4 rounded-full font-dancing text-xl"
+                onClick={() => {
+                  window.open(
+                    "https://www.youtube.com/watch?v=NNpgdo7ENUE",
+                    "_blank"
+                  );
+                }}
+              >
+                Xem Tài Liệu
+              </button>
+              <button
+                className="p-4 bg-red-300 mx-4 rounded-full font-dancing text-xl"
+                onClick={() => {
+                  navigate("/");
+                }}
+              >
+                Về Trang Chủ
+              </button>
+            </div>
           )}
-        />
+        </div>
       </div>
     </div>
   );
